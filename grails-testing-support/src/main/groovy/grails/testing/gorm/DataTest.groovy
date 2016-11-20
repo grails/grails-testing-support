07e2a23c5a881500e96f093d15175439f76de7b7
Jeff Scott Brown <brownj@ociweb.com>
2016-11-19 23:37:04 -0600

    Register a cascading validator

--- grails-testing-support/src/main/groovy/grails/testing/gorm/DataTest.groovy
+++ grails-testing-support/src/main/groovy/grails/testing/gorm/DataTest.groovy
@@ -27,8 +27,8 @@ import org.grails.datastore.mapping.core.AbstractDatastore
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.model.lifecycle.Initializable
 import org.grails.testing.GrailsUnitTest
+import org.grails.testing.gorm.MockCascadingDomainClassValidator
 import org.grails.validation.ConstraintEvalUtils
-import org.grails.validation.GrailsDomainClassValidator
 import org.junit.Before
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.validation.Validator
@@ -91,7 +91,7 @@ trait DataTest extends GrailsUnitTest {
                 bean.singleton = false
                 bean.autowire = "byName"
             }
-            "$validationBeanName"(GrailsDomainClassValidator) { bean ->
+            "$validationBeanName"(MockCascadingDomainClassValidator) { bean ->
                 getDelegate().messageSource = ref("messageSource")
                 bean.lazyInit = true
                 getDelegate().domainClass = domain
