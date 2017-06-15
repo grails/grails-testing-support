fc130ded4db0e16af2f616fdc17c2d2eb5e6b981
jameskleeh <james.kleeh@gmail.com>
2017-06-15 16:29:15 -0400

    Remove FreshRuntime in favor of spocks @ConfineMetaClassChanges

--- grails-testing-support/src/main/groovy/org/grails/testing/spock/TestingSupportExtension.groovy
+++ grails-testing-support/src/main/groovy/org/grails/testing/spock/TestingSupportExtension.groovy
@@ -11,24 +11,12 @@ import org.spockframework.runtime.model.SpecInfo
 class TestingSupportExtension extends AbstractGlobalExtension {
 
     AutowiredInterceptor autowiredInterceptor = new AutowiredInterceptor()
-    FreshRuntimeInterceptor freshRuntimeInterceptor = new FreshRuntimeInterceptor()
     CleanupContextInterceptor cleanupContextInterceptor = new CleanupContextInterceptor()
 
     @Override
     void visitSpec(SpecInfo spec) {
         if (AutowiredTest.isAssignableFrom(spec.reflection)) {
             spec.addSetupInterceptor(autowiredInterceptor)
-        }
-        if (spec.reflection.getAnnotation(FreshRuntime) != null) {
-            spec.allFeatures.each {
-                it.featureMethod.addInterceptor(freshRuntimeInterceptor)
-            }
-        } else {
-            spec.allFeatures.each {
-                if (it.featureMethod.getAnnotation(FreshRuntime) != null) {
-                    it.featureMethod.addInterceptor(freshRuntimeInterceptor)
-                }
-            }
         }
         if (GrailsUnitTest.isAssignableFrom(spec.reflection)) {
             spec.addCleanupSpecInterceptor(cleanupContextInterceptor)
