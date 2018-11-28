3975735c461596640adad3a945cf0f8ba0f2d17a
Iván López <lopezi@objectcomputing.com>
2018-11-28 15:59:26 +0100

    Fix compilation errors

--- grails-gorm-testing-support/src/main/groovy/org/grails/testing/gorm/spock/GormTestingSupportExtension.groovy
+++ grails-gorm-testing-support/src/main/groovy/org/grails/testing/gorm/spock/GormTestingSupportExtension.groovy
@@ -1,14 +1,8 @@
 package org.grails.testing.gorm.spock
 
 import grails.testing.gorm.DataTest
-import grails.validation.ConstrainedProperty
 import groovy.transform.CompileStatic
-import org.grails.datastore.mapping.core.DatastoreUtils
-import org.grails.datastore.mapping.reflect.ClassPropertyFetcher
-import org.grails.datastore.mapping.simple.SimpleMapDatastore
 import org.spockframework.runtime.extension.AbstractGlobalExtension
-import org.spockframework.runtime.extension.IMethodInterceptor
-import org.spockframework.runtime.extension.IMethodInvocation
 import org.spockframework.runtime.model.SpecInfo
 
 @CompileStatic
