864287af6587ee91c16582409c0067ad768eb3ae
James Daugherty <jdaugherty@jdresources.net>
2024-11-08 11:04:08 -0500

    Convert to httpclient instead of geb test since it's just downloading the page

--- examples/demo33/src/integration-test/groovy/demo/JsonControllerSpec.groovy
+++ examples/demo33/src/integration-test/groovy/demo/JsonControllerSpec.groovy
@@ -1,18 +1,29 @@
 package demo
 
-import grails.plugin.geb.ContainerGebSpec
 import grails.testing.mixin.integration.Integration
+import spock.lang.Specification
+
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
 
 @Integration
-class JsonControllerSpec extends ContainerGebSpec {
+class JsonControllerSpec extends Specification {
 
     void "test a json view is rendered"() {
+        given:
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:$serverPort/json/index"))
+            .timeout(Duration.ofMinutes(2))
+            .header("Accept", "application/json")
+            .GET()
+            .build()
+
         when:
-        go '/json/index'
+        def response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString())
 
         then:
-        downloadBytes() { HttpURLConnection connection ->
-            connection.setRequestProperty('Accept', 'application/json')
-        } == '{"foo":"bar"}'.bytes
+        response.body() == '{"foo":"bar"}'
     }
 }
